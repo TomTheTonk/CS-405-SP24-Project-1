6f47096a5836c6033909a11752127581da11d6b7
Thomas M Donnelly <tommymdonnelly@gmail.com>
2024-01-30 09:49:07 -0600

    fileOpener
    Opens and writes a file to a list of lists

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ def CPUUsage():
 
 #Interface
 
+#File Processing
+def fileOpen(fileName):
+        
+        operationList = []
+        file = open(fileName)
+        for line in file:
+            name = file.readline.split(0)
+            arrival = file.readline.split(0)
+            priority = file.readline.split(0)
+            CPUBurst = file.readline.split(0)
+            IOBurst = file.readline.split(0)
+            operationList.append[[name, arrival, priority, CPUBurst, IOBurst]]
+        return file
+
+
+
